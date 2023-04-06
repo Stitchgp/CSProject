6418005f12b977202d7b556afdd66f79ec32232b
Stitchgp <122938123+Stitchgp@users.noreply.github.com>
2023-04-06 00:49:16 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+df = pd.read_csv('villagers.csv')
+df.head(20)
+
+df['Name'].count()
+
+sns.catplot(x="Personality",  kind="count", hue='Gender', data=df)
+plt.show()
+
+Species = df.Species.value_counts().plot(kind = 'bar', color= "Blue", figsize = (12, 6))
+plt.xlabel("Different Species")
+plt.ylabel= ("quantity")
+plt.show()
